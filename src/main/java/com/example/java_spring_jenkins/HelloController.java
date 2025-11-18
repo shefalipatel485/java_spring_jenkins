c2bbd864c5649d42bfd3a264db727a770908d483
shefali patel <shefalip2206@gmail.com>
2025-11-18 23:17:30 +0530

    Update HelloController.java

--- src/main/java/com/example/java_spring_jenkins/HelloController.java
+++ src/main/java/com/example/java_spring_jenkins/HelloController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	  @GetMapping("/")
 	    public String hello() {
-	        return "Hello World";
+	        return "Hello World java";
 	    }
 }
+
 
 
 
